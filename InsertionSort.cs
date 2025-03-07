bd3ccfbcb88faeec8efedd2393f79dfac0a08722
mihuwis <mihuwis@gmail.com>
2025-03-07 22:28:53 +0100

    Add insertion sort

---
+++ InsertionSort.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmEfficiency
+{
+    public class InsertionSort
+    {
+        public void SortArray(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; ++i)
+            {
+                int key = arr[i];
+                int j = i - 1;
+
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j = j - 1;
+
+                }
+                arr[j + 1] = key;
+            }
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AlgorithmEfficiency;
+
+InsertionSort insertionSort = new InsertionSort();
+
+int[] arr = { 12, 11, 13, 5, 6 };
+
+insertionSort.SortArray(arr);
+
+foreach (int i in arr)
+{
+    Console.WriteLine(i);
+}
