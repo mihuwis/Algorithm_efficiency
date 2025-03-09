f470a4fd057bea13380898090bd64fd6f484cdb1
mihuwis <mihuwis@gmail.com>
2025-03-09 22:59:51 +0100

    Add benchmark class

--- Program.cs
+++ Program.cs
@@ -1,35 +1,6 @@
 ﻿using AlgorithmEfficiency;
-
-
-int[] arr01 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
-int[] arr02 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
-int[] arr03 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
-int[] arr04 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
-
-
-InsertionSort insertionSort = new InsertionSort();
-MergeSort mergeSort = new MergeSort(arr02);
-QuickSort quickSort = new QuickSort(arr03);
-
-// Sortwoanie insertion sortem
-
-insertionSort.SortArray(arr01);
-Console.WriteLine("Insert join");
-Console.WriteLine(string.Join(", ", arr01));
-
-// sortowanie Merge sortem 
-
-mergeSort.SortArray(0, arr02.Length -1);
-Console.WriteLine("Merge join: ");
-Console.WriteLine(string.Join(", ", arr02));
-
-// Sortowanie Quick sortem 
-
-quickSort.SortArray(0,arr03.Length -1);
-Console.WriteLine("Sort quick");
-Console.WriteLine(string.Join(", ", arr03));
-
-// Sortowanie Quick sortem wbudowanym (heurystycznym)
+using BenchmarkDotNet.Running;
 
 
 
+BenchmarkRunner.Run<SortingBenchmark>(); // Uruchomienie benchmarków
---
+++ SortingBenchmark.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+
+namespace AlgorithmEfficiency
+{
+    [SimpleJob]
+    public class SortingBenchmark
+    {
+        private int[] arrInsertionSort;
+        private int[] arrMergeSort;
+        private int[] arrQuickSort;
+        private int[] arrQuickSortHeuristic;
+
+        [Params(10, 1000, 100000)] 
+        public int ArraySize;
+
+        [Params("random", "sorted", "reversed", "almost sorted", "few unique")] 
+        public string DataType;
+
+        [GlobalSetup] 
+        public void Setup()
+        {
+            int[] baseArray = DataType switch
+            {
+                "random" => DataGenerator.GenerateRandomArray(ArraySize),
+                "sorted" => DataGenerator.GenerateSortedArray(ArraySize),
+                "reversed" => DataGenerator.GenerateReversedArray(ArraySize),
+                "almost sorted" => DataGenerator.GenerateAlmostSortedArray(ArraySize),
+                "few unique" => DataGenerator.GenerateFewUniqueArray(ArraySize),
+                _ => throw new ArgumentException("Nieznany typ danych")
+            };
+
+            arrInsertionSort = (int[])baseArray.Clone(); // Tworzymy kopie tablicy dla każdego algorytmu
+            arrMergeSort = (int[])baseArray.Clone();
+            arrQuickSort = (int[])baseArray.Clone();
+            arrQuickSortHeuristic = (int[])baseArray.Clone();
+        }
+
+        [Benchmark] 
+        public void InsertionSortBenchmark()
+        {
+            InsertionSort insertionSort = new InsertionSort();
+            insertionSort.SortArray(arrInsertionSort);
+        }
+
+        [Benchmark] 
+        public void MergeSortBenchmark()
+        {
+            MergeSort mergeSort = new MergeSort(arrMergeSort);
+            mergeSort.SortArray(0, arrMergeSort.Length - 1);
+        }
+
+        [Benchmark] 
+        public void QuickSortBenchmark()
+        {
+            QuickSort quickSort = new QuickSort(arrQuickSort);
+            quickSort.SortArray(0, arrQuickSort.Length - 1);
+        }
+
+        [Benchmark]
+        public void QuickSortHeuristicBenchmark()
+        {
+            Array.Sort(arrQuickSortHeuristic);
+        }
+    }
+}
