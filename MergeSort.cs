0de042a561d009ca5e43de53bddb2f0ead9f6426
mihuwis <mihuwis@gmail.com>
2025-03-09 08:56:55 +0100

    Add merge sort

---
+++ MergeSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmEfficiency
+{
+    public class MergeSort
+    {
+        private int[] array;
+
+        public MergeSort(int[] inputArray)
+        {
+            this.array = inputArray;
+        }
+
+        private void Merge(int left, int middle, int right)
+        {
+            int leftSize = middle - left + 1;
+            int rightSize = right - middle;
+
+            int[] leftArray = new int[leftSize];
+            int[] rightArray = new int[rightSize];
+
+            for (int i = 0; i < leftSize; i++)
+                leftArray[i] = array[left + i];
+            for (int j = 0; j < rightSize; j++)
+                rightArray[j] = array[middle + 1 + j];
+
+            int leftIndex = 0, rightIndex = 0, mergedIndex = left;
+
+            while (leftIndex < leftSize && rightIndex < rightSize)
+            {
+                if (leftArray[leftIndex] <= rightArray[rightIndex])
+                {
+                    array[mergedIndex] = leftArray[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    array[mergedIndex] = rightArray[rightIndex];
+                    rightIndex++;
+                }
+                mergedIndex++;
+            }
+
+            while (leftIndex < leftSize)
+            {
+                array[mergedIndex] = leftArray[leftIndex];
+                leftIndex++;
+                mergedIndex++;
+            }
+
+            while (rightIndex < rightSize)
+            {
+                array[mergedIndex] = rightArray[rightIndex];
+                rightIndex++;
+                mergedIndex++;
+            }
+        }
+
+        public void SortArray(int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = left + (right - left) / 2;
+
+                SortArray(left, middle);
+                SortArray(middle + 1, right);
+                Merge(left, middle, right);
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,22 @@
 ﻿using AlgorithmEfficiency;
 
+
+int[] arr01 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
+int[] arr02 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
+int[] arr03 = { 12, 11, 13, 5, 6, 22, 73, 3, 8, 7, 8, 8, 92, 773, 1, 0, 7, 21, 21 };
+
+
 InsertionSort insertionSort = new InsertionSort();
+MergeSort mergeSort = new MergeSort(arr02);
 
-int[] arr = { 12, 11, 13, 5, 6 };
+// Sortwoanie insertion sortem
 
-insertionSort.SortArray(arr);
+insertionSort.SortArray(arr01);
+Console.WriteLine("Insert join");
+Console.WriteLine(string.Join(", ", arr01));
 
-foreach (int i in arr)
-{
-    Console.WriteLine(i);
-}
+// sortowanie Merge sortem 
+
+mergeSort.SortArray(0, arr02.Length -1);
+Console.WriteLine("Merge join: ");
+Console.WriteLine(string.Join(", ", arr02));
